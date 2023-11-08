a3a4baeb8e7046a39f5647ea7f44041f58cd34ec
jrvazquezantelo <jrvazquezantelo@gmail.com>
2023-11-08 22:39:25 +0100

    test 2

--- lambda_function.py
+++ lambda_function.py
@@ -3,4 +3,7 @@ import pandas as pd
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
-    return event
+    print(df)
+    print('Success x1.3')
+    return "ok"
+    
